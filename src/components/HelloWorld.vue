2bcfbb02d78b77090bcb8dbc4921a80fdce6f2ab
Ethifinance <j.boyer69003@gmail.com>
2020-11-04 06:04:55 +0100

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,129 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+    </v-row>
+
+    <h1>Hello la promo Ynov !</h1>
+    <h3>Bienvenue sur Ynov Songs :)</h3>
+
+    <button @click="loaded = true">Chargement des Todos</button>
+    <Todos v-if="loaded"></Todos>
+
+    <p>Nb de count: {{ count }}</p>
+    <button @click="add" class="btn">Ajouter une plus !</button>
+    <button @click="remove" class="btn">Diminuer une plus !</button>
+
+    <p v-if="count >= 10">Win!</p>
+    <p v-else-if="count > 5">Win!!!!!!</p>
+    <p v-else>Loose!</p>
+
+    <h1 v-show="count > 10">Hello!</h1>
+
+    <h4>Prénom:</h4>
+
+    <input v-model="nbDisplay" type="range" min="0" max="6" />
+    <input
+      v-model="search"
+      type="search"
+      required
+      placeholder="recherche un prénom"
+    />
+    <p :class="colorNb">
+      Nb de prénom<span v-if="prenoms.length > 1">s</span>: {{ prenoms.length }}
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <input
+      v-model="prenom"
+      type="text"
+      required
+      placeholder="Ajouter un prénom"
+    />
+    <button @click="ajouterPrenom">Ajouter</button>
+    <p>Nb de stars: {{ nbLike }} likes</p>
+    <p v-if="goodColor">Vous êtes une star !</p>
+    <Liste :filterPrenoms="filterPrenoms">
+      <template v-slot:header>
+        <h1>Liste des {{ filterPrenoms.length }} prénoms</h1>
+      </template>
+    </Liste>
+  </v-container>
 </template>
 
 <script>
+import Liste from "./Liste"; // 1 import
+import Todos from "./Todos";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data: function () {
+    return {
+      loaded: false,
+      count: 0,
+      colorNb: "blue",
+      prenom: "",
+      search: "",
+      nbDisplay: 3,
+      prenoms: ["Steven", "Julien", "Guillaume", "Baptiste"],
+    };
+  },
+  methods: {
+    load() {
+      this.loaded = true;
+    },
+    ajouterPrenom() {
+      this.prenoms.push(this.prenom);
+      this.prenom = "";
+      if (this.prenoms.length >= 15) {
+        this.colorNb = "red";
+      }
+    },
+    add() {
+      this.count++;
+    },
+    remove() {
+      this.count--;
+    },
+  },
+  components: {
+    Liste, // 2 use
+    Todos,
+  },
+  computed: {
+    nbLike() {
+      return this.prenoms.length * this.count;
+    },
+    goodColor() {
+      return this.nbLike > 30;
+    },
+    filterPrenoms() {
+      if (this.search.length < 3) {
+        return this.prenoms;
+      } else {
+        return this.prenoms.filter((elt) => elt.includes(this.search));
+      }
+    },
+  },
+  watch: {
+    nbLike(val) {
+      if (val > 40) {
+        this.prenoms = [];
+      }
+    },
+    nbDisplay(val) {
+      this.prenoms = this.prenoms.slice(0, val);
+      if (val == 0) {
+        this.prenoms = ["Steven", "Julien", "Guillaume", "Baptiste"];
+      }
+    },
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul v-if="todos.length > 0">
+      <li v-for="(todo, index) in todos" :key="index">{{ todo.title }}</li>
+    </ul>
+    <p v-else>Chargement...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  created() {
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+      .then((response) => response.json())
+      .then((json) => (this.todos = json));
+  },
+};
+</script>
